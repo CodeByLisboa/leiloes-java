6058548c889d308039e4d9af9e6d48fa65f00469
CodeByLisboa <gabriel.lisboaa07@gmail.com>
2026-03-17 15:53:50 -0300

    Commit #4: ajustes finais e correção de bugs, código padronizado MVC

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,3 +1,4 @@
+
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,73 +7,73 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public Connection conectaBD(){
+
+    public Connection conectaBD() {
         try {
             conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/uc11", "root", ""
+                    "jdbc:mysql://localhost:3306/uc11?useSSL=false&serverTimezone=UTC",
+                    "root",
+                    ""
             );
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao conectar: " + e.getMessage());
         }
         return conn;
     }
-    
-    public void cadastrarProduto (ProdutosDTO produto){
+
+    public void cadastrarProduto(ProdutosDTO produto) {
         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
-        
+
         try {
             conn = conectaBD();
-            
+
             prep = conn.prepareStatement(sql);
             prep.setString(1, produto.getNome());
             prep.setInt(2, produto.getValor());
             prep.setString(3, produto.getStatus());
-            
+
             prep.executeUpdate();
-            
-            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
-            
+
             conn.close();
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
         }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
         listagem = new ArrayList<>();
-        
+
         String sql = "SELECT * FROM produtos";
-        
+
         try {
             conn = conectaBD();
-            
+
             prep = conn.prepareStatement(sql);
             resultset = prep.executeQuery();
-            
-            while(resultset.next()){
+
+            while (resultset.next()) {
                 ProdutosDTO produto = new ProdutosDTO();
-                
+
                 produto.setId(resultset.getInt("id"));
                 produto.setNome(resultset.getString("nome"));
                 produto.setValor(resultset.getInt("valor"));
                 produto.setStatus(resultset.getString("status"));
-                
+
                 listagem.add(produto);
             }
-            
+
             conn.close();
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro ao listar: " + e.getMessage());
         }
-        
+
         return listagem;
     }
 }
